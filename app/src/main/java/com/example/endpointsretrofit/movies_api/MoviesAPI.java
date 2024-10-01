92468d9fd97f99c446e514c3e2fb3e3548e441df
DaniNavarro <a27666@svalero.com>
2024-10-01 20:21:00 +0200

    FinalV2

--- app/src/main/java/com/example/endpointsretrofit/movies_api/MoviesAPI.java
+++ app/src/main/java/com/example/endpointsretrofit/movies_api/MoviesAPI.java
@@ -17,7 +17,7 @@ public interface MoviesAPI {
     Call<MovieResponse> getMovie();
 
     //ROUTERS!!!! express.js
-    @GET("movie/550?api_key=61e0d26ead78a0b630a6ffe401e15a6a&language=es-ES\n")
+    @GET("movie/550?api_key=eb7838fd57f27240510af0fa7f46d678&language=es-ES\n")
     Call<Movie> getDetallesMovie();
 
 }
